262d188a6539219642d8b58bb5127b964808e81e
uno9 <ws060m@gmail.com>
2021-03-27 17:37:07 +0000

    [change]fix controller

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,81 +1,45 @@
-module Api
-  module V1
-    class PostsController < ApplicationController
-      before_action :set_post, only: [:show, :edit, :update, :destroy]
+class Api::V1::PostsController < ApplicationController
 
-      def index
-        @posts = Post.order(created_at: :desc)
-        render json: { 
-          status: 'SUCCESS', 
-          message: 'Loaded posts', 
-          data: @posts
-        }
-      end
+  def index
+    @post = Post.all
+    render json: @post
+  end
 
-      def show
-        render json: { 
-          status: 'SUCCESS', 
-          message: 'Loaded the post', 
-          data: @post 
-        }
-      end
+  def show
+    @post = Post.find(params[:id])
+    render json: @post
+  end
 
-      def create
-        post = Post.new(
-          title: post_params[:title],
-          description: post_params[:description],
-          limit: post_params[:limit]
-        )
+  def create
+    @post = Post.new(**posts_params)
 
-        if post.save
-          render json: { 
-            status: 'SUCCESS', 
-            message: 'Post title', 
-            data: post
-          }
-        else
-          render json: { 
-            status: 'ERROR', 
-            message: 'Not Post title', 
-            data: post.errors 
-          }
-        end
-      end
-
-      def destroy
-        @post.destroy
-        render json: { 
-          status: 'SUCCESS', 
-          message: 'Deleted the post', 
-          data: @post 
-        }
-      end
-
-      def update
-        if @post.update(post_params)
-          render json: { 
-            status: 'SUCCESS', 
-            message: 'Updated the post', 
-            data: @post
-          }
-        else
-          render json: { 
-            status: 'ERROR', 
-            message: 'Not updated', 
-            data: @post.errors 
-          }
-        end
-      end
-
-      private
-
-      def set_post
-        @post = Post.find(params[:id])
-      end
-
-      def post_params
-        params.require(:post).permit(:title, :description, :limit)
-      end
+    if @post.save
+    else
+      render 'new'
     end
   end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(posts_params)
+      render json: @post
+    else
+      render json: @post.errors 
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    render json: { 
+      status: 'SUCCESS', 
+      message: 'Deleted the post'
+    }
+  end
+
+  private
+
+    def posts_params
+      params.require(:post).permit(:title, :description, :limit)
+    end
 end
